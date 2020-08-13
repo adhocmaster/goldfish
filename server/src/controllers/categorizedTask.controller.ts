914e5eb18aa3b8eeb91266c715e438b30ffeefcd
adhocmaster <adhocmaster@live.com>
2020-08-12 17:47:32 -0700

    Individual category add/update working.

--- server/src/controllers/categorizedTask.controller.ts
+++ server/src/controllers/categorizedTask.controller.ts
@@ -6,8 +6,8 @@ import {
     repository,
     Where,
     WhereBuilder,
-  } from '@loopback/repository';
-  import {
+} from '@loopback/repository';
+import {
     post,
     param,
     get,
@@ -17,53 +17,106 @@ import {
     del,
     requestBody,
     HttpErrors,
-  } from '@loopback/rest';
-  import {Week, CategorizedTasks} from '../models';
-  import {WeekRepository} from '../repositories';
-  import { authenticate } from '@loopback/authentication';
-  import {SecurityBindings, securityId, UserProfile} from '@loopback/security';
-  import { inject } from '@loopback/core';
-  import { UserServiceBindings } from '@loopback/authentication-jwt';
-  import { CustomUserService } from '../services/user.service';
+} from '@loopback/rest';
+import { Week, CategorizedTasks, Category } from '../models';
+import { WeekRepository } from '../repositories';
+import { authenticate } from '@loopback/authentication';
+import { SecurityBindings, securityId, UserProfile } from '@loopback/security';
+import { inject } from '@loopback/core';
+import { UserServiceBindings } from '@loopback/authentication-jwt';
+import { CustomUserService } from '../services/user.service';
 import { Bindings } from '../bindings';
 import { WeekService } from '../services/week.service';
-  
-  @authenticate('jwt')
-  export class CategorizedTaskController {
+
+@authenticate('jwt')
+export class CategorizedTaskController {
     constructor(
-      @repository(WeekRepository)
-      public weekRepository : WeekRepository,
-      @inject(Bindings.WEEK_SERVICE)
-      public weekService : WeekService,
-      @inject(UserServiceBindings.USER_SERVICE)
-      public userService : CustomUserService
-    ) {}
-    
+        @repository(WeekRepository)
+        public weekRepository: WeekRepository,
+        @inject(Bindings.WEEK_SERVICE)
+        public weekService: WeekService,
+        @inject(UserServiceBindings.USER_SERVICE)
+        public userService: CustomUserService
+    ) { }
+
 
     @patch('/weeks/all-categories/{weekId}', {
         responses: {
-          '200': {
-            description: 'Week PATCH success',
-            content: "{}"
-          },
+            '200': {
+                description: 'Week PATCH success',
+                content: { 'application/json': { schema: getModelSchemaRef(Week) } },
+            },
         },
-      })
-      async updateAllByWeekId(
+    })
+    async updateAllByWeekId(
         @inject(SecurityBindings.USER)
         currentUserProfile: UserProfile,
         @param.path.string('weekId') weekId: string,
         @requestBody({
-          content: {
-            'application/json': {
-              schema: getModelSchemaRef(CategorizedTasks, {partial: true}),
+            content: {
+                'application/json': {
+                    schema: getModelSchemaRef(CategorizedTasks, { partial: true }),
+                },
             },
-          },
         })
-        categorizedTasks: CategorizedTasks[],
-      ): Promise<any> {
-    
-        return this.weekService.updateCategorizedTasks(currentUserProfile, weekId, categorizedTasks);
-    
+        categorizedTasksArr: CategorizedTasks[],
+    ): Promise<Week> {
+
+        return this.weekService.updateCategorizedTasksArr(currentUserProfile, weekId, categorizedTasksArr);
+
     }
 
-  }
+    @patch('/weeks/category/{weekId}', {
+        responses: {
+            '200': {
+                description: 'Week PATCH success',
+                content: { 'application/json': { schema: getModelSchemaRef(Week) } },
+            },
+        },
+    })
+    async updateByWeekIdAndId(
+        @inject(SecurityBindings.USER)
+        currentUserProfile: UserProfile,
+        @param.path.string('weekId') weekId: string,
+        @requestBody({
+            content: {
+                'application/json': {
+                    schema: getModelSchemaRef(CategorizedTasks, { partial: true }),
+                },
+            },
+        })
+        category: CategorizedTasks,
+    ): Promise<Week> {
+
+        return this.weekService.updateCategory(currentUserProfile, weekId, category);
+
+    }
+
+    @post('/weeks/category/{weekId}', {
+        responses: {
+            '200': {
+                description: 'Week model instance',
+                content: { 'application/json': { schema: getModelSchemaRef(Week) } },
+            },
+        },
+    })
+    async create(
+        @inject(SecurityBindings.USER)
+        currentUserProfile: UserProfile,
+        @param.path.string('weekId') weekId: string,
+        @requestBody({
+            content: {
+                'application/json': {
+                    schema: getModelSchemaRef(CategorizedTasks, { partial: true }),
+                },
+            },
+        })
+        category: CategorizedTasks,
+    ): Promise<Week> {
+
+        return this.weekService.addCategory(currentUserProfile, weekId, category);
+
+    }
+
+
+}
--- server/src/services/week.service.ts
+++ server/src/services/week.service.ts
@@ -3,72 +3,72 @@ import { repository } from '@loopback/repository';
 import { inject } from '@loopback/core';
 import { UserServiceBindings } from '@loopback/authentication-jwt';
 import { CustomUserService } from './user.service';
-import {SecurityBindings, securityId, UserProfile} from '@loopback/security';
+import { SecurityBindings, securityId, UserProfile } from '@loopback/security';
 import { CategorizedTasks, Week } from '../models';
 import { HttpErrors } from '@loopback/rest';
 
 export class WeekService {
 
-    
-  constructor(
-    @repository(WeekRepository)
-    public weekRepository : WeekRepository,
-    @inject(UserServiceBindings.USER_SERVICE)
-    public userService : CustomUserService
-  ) {}
 
-  public async updateCategorizedTasks(currentUserProfile: UserProfile, weekId: string, categorizedTasks: CategorizedTasks[] ) {
+    constructor(
+        @repository(WeekRepository)
+        public weekRepository: WeekRepository,
+        @inject(UserServiceBindings.USER_SERVICE)
+        public userService: CustomUserService
+    ) { }
 
-    
-    if (await this.weekRepository.canEdit(weekId, currentUserProfile[securityId])) {
-        
-        let week = await this.weekRepository.findById(weekId);
-        week.categorizedTasks = categorizedTasks;
+    public async updateCategorizedTasksArr(currentUserProfile: UserProfile, weekId: string, categorizedTasksArr: CategorizedTasks[]) {
 
-        return await this.updateByIdWithoutSecurityCheck(weekId, week);
-        
 
-    } else {
-        throw new HttpErrors.Unauthorized("Week not accessible");
-    }
+        if (await this.weekRepository.canEdit(weekId, currentUserProfile[securityId])) {
 
-  }
+            let week = await this.weekRepository.findById(weekId);
+            week.categorizedTasks = categorizedTasksArr;
 
-  protected reCalculateTImes(week: Week) {
+            return await this.updateByIdWithoutSecurityCheck(weekId, week);
 
-    if (!week.categorizedTasks) {
-        return;
-    }
 
-    let plannedMinutes = 0;
-    let completedMinutes = 0;
-
-    let catIndex: number;
-    for (catIndex = 0; catIndex < week.categorizedTasks?.length; ++catIndex) {
-
-        let catTasks = week.categorizedTasks[catIndex];
-        plannedMinutes += catTasks.totalMinutes ?? 0;
-        completedMinutes += catTasks.completedMinutes ?? 0;
+        } else {
+            throw new HttpErrors.Unauthorized("Week not accessible");
+        }
 
     }
 
-    week.plannedMinutes = plannedMinutes;
-    week.completedMinutes = completedMinutes;
-    
-  }
+    protected reCalculateTImes(week: Week) {
 
-  public async updateById(userProfile: UserProfile, id: string, week: Week) {
+        if (!week.categorizedTasks) {
+            return;
+        }
 
-    if (await this.weekRepository.canEdit(id, userProfile[securityId])) {
-        
-        return await this.updateByIdWithoutSecurityCheck(id, week);
+        let plannedMinutes = 0;
+        let completedMinutes = 0;
 
-    } else {
-        throw new HttpErrors.Unauthorized("Week not accessible");
+        let catIndex: number;
+        for (catIndex = 0; catIndex < week.categorizedTasks?.length; ++catIndex) {
+
+            let catTasks = week.categorizedTasks[catIndex];
+            plannedMinutes += catTasks.totalMinutes ?? 0;
+            completedMinutes += catTasks.completedMinutes ?? 0;
+
+        }
+
+        week.plannedMinutes = plannedMinutes;
+        week.completedMinutes = completedMinutes;
+
     }
 
+    public async updateById(userProfile: UserProfile, id: string, week: Week) {
 
-  }
+        if (await this.weekRepository.canEdit(id, userProfile[securityId])) {
+
+            return await this.updateByIdWithoutSecurityCheck(id, week);
+
+        } else {
+            throw new HttpErrors.Unauthorized("Week not accessible");
+        }
+
+
+    }
 
 
     private async updateByIdWithoutSecurityCheck(id: string, week: Week) {
@@ -76,5 +76,74 @@ export class WeekService {
         this.reCalculateTImes(week); // TODO do this at client end.
         await this.weekRepository.updateById(id, week);
         return this.weekRepository.findById(id);
+    }
+
+    public async addCategory(currentUserProfile: UserProfile, weekId: string, category: CategorizedTasks) {
+        
+        if (await this.weekRepository.canEdit(weekId, currentUserProfile[securityId])) {
+
+            let week = await this.weekRepository.findById(weekId);
+            if (week.categorizedTasks) {
+
+                if (this.categoryAlreadyExists(week, category)) {
+                    throw new HttpErrors.Conflict("Category already exists in the week.");
+                }
+                week.categorizedTasks?.push(category);
+            }
+            else
+                week.categorizedTasks = [category];
+
+            return await this.updateByIdWithoutSecurityCheck(weekId, week);
+
+        } else {
+            throw new HttpErrors.Unauthorized("Week not accessible");
+        }
+
+    }
+
+    public categoryAlreadyExists(week: Week, category: CategorizedTasks) {
+
+        if (!week.categorizedTasks) {
+            return false;
+        }
+
+        for (let existingCat  of week.categorizedTasks) {
+
+            if (existingCat.categoryId ==  category.categoryId) {
+                return true;
+            }
+            
+        }
+
+        return false;
+
+    }
+
+    public async updateCategory(currentUserProfile: UserProfile, weekId: string, category: CategorizedTasks) {
+        
+        if (await this.weekRepository.canEdit(weekId, currentUserProfile[securityId])) {
+
+            let week = await this.weekRepository.findById(weekId);
+            if (week.categorizedTasks) {
+
+                let catIndex: number;
+                for (catIndex = 0; catIndex < week.categorizedTasks?.length; ++catIndex) {
+        
+                    let existingCat = week.categorizedTasks[catIndex];
+
+                    if ( existingCat.categoryId == category.categoryId ) {
+                        week.categorizedTasks[catIndex] = category;
+                        return await this.updateByIdWithoutSecurityCheck(weekId, week);
+                    }
+                }
+        
+            }
+            
+            throw new HttpErrors.NotFound("No such category exists in the week.")
+
+        } else {
+            throw new HttpErrors.Unauthorized("Week not accessible");
+        }
+
     }
 }
